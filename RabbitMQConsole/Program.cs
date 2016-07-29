513b7f75bed40115e19479f290fdc3b39ea3a22c
vamanpnayak <vamanpnayak@gmail.com>
2016-07-29 17:29:16 +0200

    Add project files.

---
+++ RabbitMQConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Messages;
+
+namespace RabbitMQConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("foobar"))
+            {
+                var input = "";
+                Console.WriteLine("Enter a message 'quit' to close");
+                while ((input = Console.ReadLine()) != "quit")
+                {
+                    bus.Publish(new MyMessage{ Text = input});
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQConsoleSubscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Messages;
+
+namespace RabbitMQConsoleSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("foobar"))
+            {
+                bus.Subscribe<MyMessage>("Test", HandleTextMessage);
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+        static void HandleTextMessage(MyMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
